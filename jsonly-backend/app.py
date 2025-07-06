bc77a84cf9933bbe77869e6d383d4e8d56685456
Karim Lahrichi <karimlahrichi@hotmail.com>
2025-07-06 13:54:49 -0400

    Fix for backend prod

--- jsonly-backend/app.py
+++ jsonly-backend/app.py
@@ -436,7 +436,8 @@ async def read_root():
     return {"message": "FastAPI backend is running"}
 
 def main():
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
 
 if __name__ == '__main__':
     main()
